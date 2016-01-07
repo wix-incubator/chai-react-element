99a62437d48c8f932d16950a50728ee36812f15a
AviVahl <avi.vahl@wix.com>
2016-01-07 15:31:07 +0200

    Refactor logic to a separate function

--- src/can-be-asserted.js
+++ src/can-be-asserted.js
@@ -2,11 +2,13 @@ import _ from 'lodash';
 import React from 'react';
 
 export default function canBeAsserted(vdom) {
+    function isCompatibleObject(node) {
+        return node
+            && typeof node === 'object'
+            && typeof node.type === 'string'
+            && (!node.props || typeof node.props === 'object');
+    }
+
     return _.all([].concat(vdom), node =>
-        React.isValidElement(node) ||
-        (node
-        && typeof node === 'object'
-        && typeof node.type === 'string'
-        && (!node.props || typeof node.props === 'object'))
-    );
+        React.isValidElement(node) || isCompatibleObject(node));
 }
